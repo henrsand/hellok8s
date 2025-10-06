435b23413980e7c021011e1d8f3a9e619b410bb5
Henrik Sandström <svartvikare@gmail.com>
2025-10-06 20:57:13 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '2rem',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>Hello Kubernetes!</h1>
+    </div>
+  )
+}
+
+export default App
